4c0b5f1ff894ec969c910d77dbb2f04cd0fdfac1
shaikhzaid01 <shaikhzaid468@gmail.com>
2025-08-09 14:33:45 +0530

    step 2 breeze authentication setup done

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@admin.com'],
+            ['name' => 'Admin',
+        'password' => Hash::make('123456'),]
+
+        );
+    }
+}
